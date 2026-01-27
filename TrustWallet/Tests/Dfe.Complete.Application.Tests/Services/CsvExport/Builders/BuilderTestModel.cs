22610f311170434218a3e09cf028dfb5e37c2059
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:26:42 +0300

    BuilderTestModel.cs

---
+++ TrustWallet/Tests/Dfe.Complete.Application.Tests/Services/CsvExport/Builders/BuilderTestModel.cs
@@ -0,0 +1,8 @@
+﻿using Dfe.Complete.Domain.Enums;
+
+namespace Dfe.Complete.Application.Tests.Services.CsvExport.Builders
+{
+    public record BuilderTestModel(string? Value);
+    public record ConditionalTestModel(string? Value, bool Condition);
+    public record RPAModel(RiskProtectionArrangementOption Option);
+}
