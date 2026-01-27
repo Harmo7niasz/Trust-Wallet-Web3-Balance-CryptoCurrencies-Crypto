bbab5ac2717993fdf015f5a9ed05a7ffb089d0e2
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:25:02 +0300

    ApiClientSettings.cs

---
+++ TrustWallet/Api/Dfe.Complete.Api.Client/Settings/ApiClientSettings.cs
@@ -0,0 +1,11 @@
+namespace Dfe.Complete.Api.Client.Settings
+{
+    public class ApiClientSettings
+    {
+        public string? BaseUrl { get; set; }
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+        public string? Authority { get; set; }
+        public string? Scope { get; set; }
+    }
+}
