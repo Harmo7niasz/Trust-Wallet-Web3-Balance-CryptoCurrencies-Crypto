a11d8116f575efd6ecb47e7409613f25612eb0d3
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:26:45 +0300

    DefaultIfTests.cs

---
+++ TrustWallet/Tests/Dfe.Complete.Application.Tests/Services/CsvExport/Builders/DefaultIfTests.cs
@@ -0,0 +1,29 @@
+﻿using Dfe.Complete.Application.Services.CsvExport.Builders;
+
+namespace Dfe.Complete.Application.Tests.Services.CsvExport.Builders
+{
+    public class DefaultIfTests
+    {
+        private const string DefaultValue = "Default";
+
+        [Fact]
+        public void Build_WhenValueIsNull_ReturnsDefault()
+        {
+            var builder = new DefaultIf<ConditionalTestModel>(x => x.Condition, x => x.Value, DefaultValue);
+
+            var result = builder.Build(new ConditionalTestModel("Hello", true));
+
+            Assert.Equal(DefaultValue, result);
+        }
+
+        [Fact]
+        public void Build_WhenValueIsNotNullOrEmpty_ReturnsValueToString()
+        {
+            var builder = new DefaultIf<ConditionalTestModel>(x => x.Condition, x => x.Value, DefaultValue);
+
+            var result = builder.Build(new ConditionalTestModel("Hello", false));
+
+            Assert.Equal("Hello", result);
+        }
+    }
+}
