f79deb7b6dd9262ea4c47b5ae4285ef10cc8f2ac
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:26:36 +0300

    AgeRangeBuilderTests.cs

---
+++ TrustWallet/Tests/Dfe.Complete.Application.Tests/Services/CsvExport/Builders/AgeRangeBuilderTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using Dfe.Complete.Application.Services.CsvExport.Builders;
+using Dfe.Complete.Domain.Entities;
+using Dfe.Complete.Tests.Common.Customizations.Models;
+
+namespace Dfe.Complete.Application.Tests.Services.CsvExport.Builders
+{
+    public class AgeRangeBuilderTests
+    {
+        [Theory]
+        [InlineData(null, null, "")]
+        [InlineData(null, 11, "")]
+        [InlineData(4, null, "")]
+        [InlineData(4, 11, "4-11")]
+        [InlineData(2, 17, "2-17")]
+        public void Build_WhenCalled_ShouldReturnNull(int? ageLower, int? ageUpper, string expected)
+        {
+            GiasEstablishment establishment = new Fixture().Customize(new EstablishmentsCustomization()).Create<GiasEstablishment>();
+
+            establishment.AgeRangeLower = ageLower;
+            establishment.AgeRangeUpper = ageUpper;
+
+            var builder = new AgeRangeBuilder<GiasEstablishment>(x => x);
+
+            var result = builder.Build(establishment);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
