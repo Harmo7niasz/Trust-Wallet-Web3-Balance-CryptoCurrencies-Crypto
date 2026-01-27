f4002f7adc4190bf52b259e7ee4dba8f0b23eb8c
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:26:43 +0300

    DefaultIfEmptyTests.cs

---
+++ TrustWallet/Tests/Dfe.Complete.Application.Tests/Services/CsvExport/Builders/DefaultIfEmptyTests.cs
@@ -0,0 +1,39 @@
+﻿using Dfe.Complete.Application.Services.CsvExport.Builders;
+
+namespace Dfe.Complete.Application.Tests.Services.CsvExport.Builders
+{
+    public class DefaultIfEmptyTests
+    {
+        private const string DefaultValue = "Default";
+
+        [Fact]
+        public void Build_WhenValueIsNull_ReturnsDefault()
+        {
+            var builder = new DefaultIfEmpty<BuilderTestModel>(x => x.Value, DefaultValue);
+
+            var result = builder.Build(new BuilderTestModel(null));
+
+            Assert.Equal(DefaultValue, result);
+        }
+
+        [Fact]
+        public void Build_WhenValueIsEmpty_ReturnsDefault()
+        {
+            var builder = new DefaultIfEmpty<BuilderTestModel>(x => x.Value, DefaultValue);
+
+            var result = builder.Build(new BuilderTestModel(string.Empty));
+
+            Assert.Equal(DefaultValue, result);
+        }
+
+        [Fact]
+        public void Build_WhenValueIsNotNullOrEmpty_ReturnsValueToString()
+        {
+            var builder = new DefaultIfEmpty<BuilderTestModel>(x => x.Value, DefaultValue);
+
+            var result = builder.Build(new BuilderTestModel("Hello"));
+
+            Assert.Equal("Hello", result);
+        }
+    }
+}
