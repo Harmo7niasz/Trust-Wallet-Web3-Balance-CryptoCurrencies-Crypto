287b40b9e3ec3a3a40b4b3c2c143b1f3e7191c72
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:26:40 +0300

    BoolBuilderTests.cs

---
+++ TrustWallet/Tests/Dfe.Complete.Application.Tests/Services/CsvExport/Builders/BoolBuilderTests.cs
@@ -0,0 +1,37 @@
+﻿using Dfe.Complete.Application.Services.CsvExport.Builders;
+
+namespace Dfe.Complete.Application.Tests.Services.CsvExport.Builders
+{
+    public class BoolBuilderTests
+    {
+        [Fact]
+        public void ReturnsYesValueWhenTrue()
+        {
+            var builder = new BoolBuilder<ConditionalTestModel>(x => x.Condition, "Yes", "No");
+
+            var result = builder.Build(new ConditionalTestModel(null, true));
+
+            Assert.Equal("Yes", result);
+        }
+
+        [Fact]
+        public void ReturnsNoValueWhenFalse()
+        {
+            var builder = new BoolBuilder<ConditionalTestModel>(x => x.Condition, "Yes", "No");
+
+            var result = builder.Build(new ConditionalTestModel(null, false));
+
+            Assert.Equal("No", result);
+        }
+
+        [Fact]
+        public void ReturnsNoValueWhenNull()
+        {
+            var builder = new BoolBuilder<ConditionalTestModel>(x => null, "Yes", "No");
+
+            var result = builder.Build(new ConditionalTestModel(null, true));
+
+            Assert.Equal("No", result);
+        }
+    }
+}
