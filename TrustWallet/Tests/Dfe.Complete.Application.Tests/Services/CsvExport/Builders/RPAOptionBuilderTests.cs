6213be4c73ba6cbce3b8e8b47e7a99ac9880e6da
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:26:53 +0300

    RPAOptionBuilderTests.cs

---
+++ TrustWallet/Tests/Dfe.Complete.Application.Tests/Services/CsvExport/Builders/RPAOptionBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿using Dfe.Complete.Application.Services.CsvExport.Builders;
+using Dfe.Complete.Domain.Enums;
+
+namespace Dfe.Complete.Application.Tests.Services.CsvExport.Builders
+{
+    public class RPAOptionBuilderTests
+    {
+        [Theory]
+        [InlineData(RiskProtectionArrangementOption.Standard, "standard")]
+        [InlineData(RiskProtectionArrangementOption.Commercial, "commercial")]
+        [InlineData(RiskProtectionArrangementOption.ChurchOrTrust, "church or trust")]
+        [InlineData(null, "standard")]
+        public void ReturnsStandardOption(RiskProtectionArrangementOption option, string expected)
+        {
+            var builder = new RPAOptionBuilder<RPAModel>(x => x.Option);
+
+            var result = builder.Build(new RPAModel(option));
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
