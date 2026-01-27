d51ce4a47cf223ffccf0e166b580f1985435fb62
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:26:52 +0300

    ProvisionalDateBuilderTests.cs

---
+++ TrustWallet/Tests/Dfe.Complete.Application.Tests/Services/CsvExport/Builders/ProvisionalDateBuilderTests.cs
@@ -0,0 +1,31 @@
+﻿using Dfe.Complete.Application.Services.CsvExport.Builders;
+
+namespace Dfe.Complete.Application.Tests.Services.CsvExport.Builders
+{
+    public class ProvisionalDateBuilderTests
+    {
+        [Fact]
+        public void IfHistoryDoesNotExistUseDateOnProject()
+        {
+            var model = ConversionCsvModelFactory.Make(withSignificantDateHistory: false);
+
+            var builder = new ProvisionalDateBuilder();
+
+            var result = builder.Build(model);
+
+            Assert.Equal(model.Project.SignificantDate?.ToString("yyyy-MM-dd"), result);
+        }
+
+        [Fact]
+        public void IfHistoryExistsUseDateOnHistory()
+        {
+            var model = ConversionCsvModelFactory.Make();
+
+            var builder = new ProvisionalDateBuilder();
+
+            var result = builder.Build(model);
+
+            Assert.Equal(model.SignificantDateHistory?.PreviousDate.Value.ToString("yyyy-MM-dd"), result);
+        }
+    }
+}
