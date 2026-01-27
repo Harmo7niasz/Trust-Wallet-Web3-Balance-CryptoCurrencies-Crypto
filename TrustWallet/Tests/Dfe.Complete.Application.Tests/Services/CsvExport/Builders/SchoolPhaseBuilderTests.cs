a3cd5a85f91e0dca03b767bcda743fcbb49fce83
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:26:55 +0300

    SchoolPhaseBuilderTests.cs

---
+++ TrustWallet/Tests/Dfe.Complete.Application.Tests/Services/CsvExport/Builders/SchoolPhaseBuilderTests.cs
@@ -0,0 +1,34 @@
+﻿using Dfe.Complete.Application.Services.CsvExport.Builders;
+using Dfe.Complete.Domain.Entities;
+using Dfe.Complete.Tests.Common.Customizations.Models;
+using DfE.CoreLibs.Testing.AutoFixture.Attributes;
+
+namespace Dfe.Complete.Application.Tests.Services.CsvExport.Builders
+{
+    public class SchoolPhaseBuilderTests
+    {
+        [Theory]
+        [CustomAutoData(typeof(EstablishmentsCustomization))]
+        public void WhenNotApplicableShouldReturnSchoolType(GiasEstablishment establishment)
+        {
+            establishment.PhaseName = "Not applicable";
+
+            var builder = new SchoolPhaseBuilder<GiasEstablishment>(x => x);
+
+            var result = builder.Build(establishment);
+
+            Assert.Equal(establishment.TypeName, result);
+        }
+
+        [Theory]
+        [CustomAutoData(typeof(EstablishmentsCustomization))]
+        public void WhenCalledShouldReturnSchoolPhase(GiasEstablishment establishment)
+        {
+            var builder = new SchoolPhaseBuilder<GiasEstablishment>(x => x);
+
+            var result = builder.Build(establishment);
+
+            Assert.Equal(establishment.PhaseName, result);
+        }
+    }
+}
