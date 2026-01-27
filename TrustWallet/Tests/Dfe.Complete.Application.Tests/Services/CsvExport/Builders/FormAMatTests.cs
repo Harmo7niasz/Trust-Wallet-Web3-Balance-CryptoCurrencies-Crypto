602e5f73bc869ba0602cc76bd1260ba4a11bf232
hokuspokus <musamustafa51@icloud.com>
2026-01-27 17:26:48 +0300

    FormAMatTests.cs

---
+++ TrustWallet/Tests/Dfe.Complete.Application.Tests/Services/CsvExport/Builders/FormAMatTests.cs
@@ -0,0 +1,36 @@
+﻿using Dfe.Complete.Application.Services.CsvExport.Builders;
+using Dfe.Complete.Domain.Entities;
+using Dfe.Complete.Tests.Common.Customizations.Models;
+using DfE.CoreLibs.Testing.AutoFixture.Attributes;
+
+namespace Dfe.Complete.Application.Tests.Services.CsvExport.Builders
+{
+    public class FormAMatTests
+    {
+        [Theory]
+        [CustomAutoData(typeof(ProjectCustomization))]
+
+        public void WhenIncomingUkprnisNullIsFormAMat(Project project)
+        {
+            project.IncomingTrustUkprn = null;
+                
+            var builder = new FormAMat<Project>(x => x);
+
+            var result = builder.Build(project);
+
+            Assert.Equal("form a MAT", result);
+        }
+
+        [Theory]
+        [CustomAutoData(typeof(ProjectCustomization))]
+
+        public void WhenIncomingUkprnisNotNullIsJoinAMat(Project project)
+        {
+            var builder = new FormAMat<Project>(x => x);
+
+            var result = builder.Build(project);
+
+            Assert.Equal("join a MAT", result);
+        }
+    }
+}
